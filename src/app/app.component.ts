d5d978bab0aca02cc5b48dee085fa14a8c0f090d
ZoharNissim <zoharnissim315@gmail.com>
2021-04-25 13:40:51 +0300

    JSON file added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as data from './searchResults.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'phenom-assignment';
+  ngOnInit() {
+    console.log(data);
+  }
 }
